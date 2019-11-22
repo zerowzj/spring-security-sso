409659af558345dea39caf9defff0e81f4923d0d
zerowzj <zerowzj@163.com>
2019-11-22 21:16:20 +0800

    更新代码

--- auth-server/src/main/java/study/springsecurity/auth/server/auth/tokenstore/TokenStoreCfg.java
+++ auth-server/src/main/java/study/springsecurity/auth/server/auth/tokenstore/TokenStoreCfg.java
@@ -1,6 +1,5 @@
 package study.springsecurity.auth.server.auth.tokenstore;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

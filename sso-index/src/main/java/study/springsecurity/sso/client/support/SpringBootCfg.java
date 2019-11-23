7c20d5f2e2da7519e439774ed0437d54a0355bcf
zerowzj <zerowzj@163.com>
2019-11-23 19:49:30 +0800

    更新代码

---
+++ sso-index/src/main/java/study/springsecurity/sso/client/support/SpringBootCfg.java
@@ -0,0 +1,7 @@
+package study.springsecurity.sso.client.support;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "study.springsecurity.sso.client2")
+public class SpringBootCfg {
+}
